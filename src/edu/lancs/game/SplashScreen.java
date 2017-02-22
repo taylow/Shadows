db27ebe8dd2da2919fc85869a7ba8f9d76877244
Taylor-Woodcock <taylor.woodcock@outlook.com>
2017-02-22 06:11:56 +0000

    Bed pls

--- src/edu/lancs/game/SplashScreen.java
+++ src/edu/lancs/game/SplashScreen.java
@@ -10,6 +10,7 @@ public class SplashScreen {
     public SplashScreen() {
         window = new JWindow();
         try {
+            //TODO: Could definitely be improved. Change of image maybe? I don't know. This one looks indie though
             window.getContentPane().add(new JLabel("", new ImageIcon(new URL("https://s-media-cache-ak0.pinimg.com/originals/90/80/60/9080607321ab98fa3e70dd24b2513a20.gif")), SwingConstants.CENTER));
             window.setBounds(0, 0, 800, 600);
             window.setLocationRelativeTo(null);
