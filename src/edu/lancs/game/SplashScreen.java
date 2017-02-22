c4f68ff620e2924bb575f9099d3f14b19304d2f4
Taylor-Woodcock <taylor.woodcock@outlook.com>
2017-02-22 23:19:13 +0000

    Added lighting, fixed MiniMap, added random Enemies in rooms, etc.

--- src/edu/lancs/game/SplashScreen.java
+++ src/edu/lancs/game/SplashScreen.java
@@ -11,7 +11,7 @@ public class SplashScreen {
         window = new JWindow();
         try {
             //TODO: Could definitely be improved. Change of image maybe? I don't know. This one looks indie though
-            window.getContentPane().add(new JLabel("", new ImageIcon(new URL("https://i.imgur.com/Nge4Xzw.gif")), SwingConstants.CENTER));
+            window.getContentPane().add(new JLabel("", new ImageIcon(new URL("https://s-media-cache-ak0.pinimg.com/originals/90/80/60/9080607321ab98fa3e70dd24b2513a20.gif")), SwingConstants.CENTER));
             window.setBounds(0, 0, 800, 600);
             window.setLocationRelativeTo(null);
             window.setVisible(true);
