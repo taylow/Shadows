e6fd661af03afba090345c753323bb29e1cfaff1
Taylor-Woodcock <taylor.woodcock@outlook.com>
2017-02-22 06:00:03 +0000

    Okay, scratch that last cry for sleep, I added a splash screen template for whoever wants to do the SplashScreen. Essentially just either change the URL or add it as a proper resource (not loaded the same way, as the SplashScreen is to cover up the resource loading).

--- src/edu/lancs/game/Main.java
+++ src/edu/lancs/game/Main.java
@@ -7,7 +7,9 @@ import static edu.lancs.game.Constants.*;
 public class Main {
     public static void main(String[] args) {
         //FIXME: This whole file could be deleted, technically. It's only to create the Window, but this could be done in Window.
+        SplashScreen splashScreen = new SplashScreen();
         Window window = new Window(GAME_WIDTH, GAME_HEIGHT, GAME_TITLE, GAME_FULLSCREEN, GAME_FRAME_RATE); // creates a Window with variables in Constants
+        splashScreen.dispose();
 
         // create a MenuScene and set it as the current scene (this should always be the first scene)
         // FIXME: make this scene display first by default
---
+++ src/edu/lancs/game/SplashScreen.java
@@ -0,0 +1,26 @@
+package edu.lancs.game;
+
+import javax.swing.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SplashScreen {
+    private JWindow window;
+
+    public SplashScreen() {
+        window = new JWindow();
+        try {
+            window.getContentPane().add(new JLabel("", new ImageIcon(new URL("https://s-media-cache-ak0.pinimg.com/originals/90/80/60/9080607321ab98fa3e70dd24b2513a20.gif")), SwingConstants.CENTER));
+            window.setBounds(0, 0, 800, 600);
+            window.setLocationRelativeTo(null);
+            window.setVisible(true);
+            window.setAlwaysOnTop(true);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dispose() {
+        window.dispose();
+    }
+}
